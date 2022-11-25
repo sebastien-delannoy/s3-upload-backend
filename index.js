60330946f61df71356988dcf18d470d701e02972
sebastien-delannoy <45290590+sebastien-delannoy@users.noreply.github.com>
2022-11-25 12:25:39 +0100

    initial

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*node express app tat uploads and downloads objects from amazon S3*/
-var express = require('express');
-const cors = require( 'cors');
-var bodyParser = require('body-parser');
+var express = require("express");
+const cors = require("cors");
+var bodyParser = require("body-parser");
 var fileupload = require("express-fileupload");
-var amazonS3 = require('aws-sdk');
-const { Redshift } = require('aws-sdk');
+var amazonS3 = require("aws-sdk");
+const { Redshift } = require("aws-sdk");
 
 var app = express();
 app.use(cors());
@@ -12,46 +12,50 @@ app.use(fileupload());
 
 // Initializing the amazonS3 object.
 amazonS3.config.update({
-  accessKeyId: "YOUR_ACCESS_KEY_ID",
-  secretAccessKey: "YOUR_SECRET",
-  subregion: "YOUR_REGION"
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET,
+  subregion: process.env.REGION,
 });
 
-var s3Bucket = new amazonS3.S3({ params: { Bucket: "YOUR_BUCKET_NAME" } });
+var s3Bucket = new amazonS3.S3({
+  params: { Bucket: "sebast-bucket-together" },
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // upload route
-app.post('/api/upload', function (req, res) {
+app.post("/api/upload", function (req, res) {
+  if (req.files) {
+    var currentTime = new Date();
 
-    if (req.files) {
-        var file = req.files.file,
-            filename = file.name,
-            username=req.body.username;
-        var params = {
-            Key: `${username}/${filename}`,
-            Body: file.data,
-            ContentType: file.mimetype,
-            ACL: 'public-read'
-        };
+    var file = req.files.file,
+      filename = currentTime + file.name,
+      username = req.body.username;
 
-        // upload file to S3
-        s3Bucket.upload(params, function (err, data) {
-            if (err) {
-                console.log('Error occured while trying to upload to AWS: ', err);
-                res.send(err);
-            } else {
-                console.log("Upload Success", data.Location);
-                res.json(data);
-            }
-        });
-    } else {
-        console.log('no files')
-    }
+    var params = {
+      Key: `${username}/${filename}`,
+      Body: file.data,
+      ContentType: file.mimetype,
+      ACL: "public-read",
+    };
+
+    // upload file to S3
+    s3Bucket.upload(params, function (err, data) {
+      if (err) {
+        console.log("Error occured while trying to upload to AWS: ", err);
+        res.send(err);
+      } else {
+        console.log("Upload Success", data.Location);
+        res.json(data);
+      }
+    });
+  } else {
+    console.log("no files");
+  }
 });
 
 // app listen
 app.listen(8080, function () {
-    console.log('Working on port 8080');
+  console.log("Working on port 8080");
 });
